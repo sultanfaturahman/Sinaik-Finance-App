e5519123e97527f6b183ddbc8c2becb340b86fae
sultanfaturahman <sultannfaturahman@gmail.com>
2026-01-02 22:54:16 +0700

    fix performance

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import { ThemeProvider } from "./components/ThemeProvider";
 import App from "./App.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 if ("serviceWorker" in navigator) {
@@ -11,7 +12,10 @@ if ("serviceWorker" in navigator) {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+    <SpeedInsights />
+  </>
 );
